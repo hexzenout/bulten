682472530ccd0215fac078d755f76b7f8ef62e27
hexzenout <hexzen20@gmail.com>
2026-05-21 17:05:30 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// V47 SERVICE WORKER SELF DESTRUCT
+// V48 SERVICE WORKER SELF DESTRUCT
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => {
   event.waitUntil(
