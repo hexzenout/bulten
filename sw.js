70d140e1709550f1fd733801cdd6b5473424568b
hexzenout <hexzen20@gmail.com>
2026-05-21 10:37:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-// V46B SW SELF DESTRUCT
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
+// V46C SERVICE WORKER SELF DESTRUCT
+self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.registration.unregister())
-      .then(() => self.clients.matchAll())
-      .then(clients => clients.forEach(client => client.navigate(client.url)))
   );
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener("fetch", event => event.respondWith(fetch(event.request)));
